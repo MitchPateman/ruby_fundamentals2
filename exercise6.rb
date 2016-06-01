438c7059c17b84b1c3efe23df082f8e97c21b9a6
Mitch Pateman <mitch_pateman@hotmail.com>
2016-06-01 10:53:57 -0400

    added rice with shovel, added the sort

--- exercise6.rb
+++ exercise6.rb
@@ -1,6 +1,7 @@
-grocery_list = ["carrots", "toilet paper", "apples", "salmon", "rice"]
-
+grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
+grocery_list << 'rice'
 count = grocery_list.length
-
+grocery_list= grocery_list.sort
+puts "There are #{count} items on your list."
 puts grocery_list[0..count]
 # puts grocery_list
