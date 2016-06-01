73ca576cc84ae886f43596d9d3cd3d2089a3bc98
Mitch Pateman <mitch_pateman@hotmail.com>
2016-06-01 11:17:02 -0400

    Changed each do --> map.do (to add the *)

--- exercise6.rb
+++ exercise6.rb
@@ -1,7 +1,16 @@
 grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
-grocery_list << 'rice'
-count = grocery_list.length
-grocery_list= grocery_list.sort
-puts "There are #{count} items on your list."
-puts grocery_list[0..count]
-# puts grocery_list
+grocery_list << 'rice' #Add rice
+
+
+count = grocery_list.length #store the length of array
+grocery_list= grocery_list.sort #alphabetize
+
+puts "There are #{count} items on your list." #Output how many
+
+grocery_list.map do |item|    #Output all items
+	puts "* " + item
+end
+
+#puts grocery_list[0..count]    ####Output all items different way
+
+grocery_list.none? { |item| item == "banana"}
