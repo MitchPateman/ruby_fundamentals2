d6df15c4ba275200fb8dc972e45e7f88ef8f9df6
Mitch Pateman <mitch_pateman@hotmail.com>
2016-06-01 12:04:58 -0400

    ex 6, need to fix salmon removal and banana include?

--- exercise6.rb
+++ exercise6.rb
@@ -7,10 +7,24 @@ grocery_list= grocery_list.sort #alphabetize
 
 puts "There are #{count} items on your list." #Output how many
 
-grocery_list.map do |item|    #Output all items
+grocery_list.map do |item|    #Output all items and add the *
 	puts "* " + item
 end
 
 #puts grocery_list[0..count]    ####Output all items different way
 
-grocery_list.none? { |item| item == "banana"}
+grocery_list.include? ("banana") #Does it include banana??
+if true
+	puts "Buy Bananas"
+else
+	puts "You do not need Bananas"
+end
+
+puts "The second item on the list is #{grocery_list[1]}"
+
+grocery_list.pop #This removes the last item, TP not salmon :S
+
+puts"Here is the updated list without Salmon"
+grocery_list.map do |item|    #Output all items and add the *
+	puts "* " + item
+end
