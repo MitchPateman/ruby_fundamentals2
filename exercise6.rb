c2252e341a498f5f8156db4b10bf792e21959168
Mitch Pateman <mitch_pateman@hotmail.com>
2016-06-01 10:45:18 -0400

    added count=array.grocery_list.length

--- exercise6.rb
+++ exercise6.rb
@@ -1,4 +1,6 @@
 grocery_list = ["carrots", "toilet paper", "apples", "salmon", "rice"]
 
-puts "* " + grocery_list.join(" * ")
+count = grocery_list.length
+
+puts grocery_list[0..count]
 # puts grocery_list
